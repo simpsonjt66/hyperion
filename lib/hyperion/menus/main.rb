43cf06a183c518766d07278df55943f54c71fd2c
John Simpson <simpsonjt66@gmail.com>
2026-05-28 13:15:46 -0400

    Fix naming inconsistency in config file.

--- lib/hyperion/menus/main.rb
+++ lib/hyperion/menus/main.rb
@@ -5,12 +5,12 @@ module Menus
   class Main
     def self.show
       menu_options = OPTIONS[:main_menu]
-      prompts = menu_options.map { |item| item[:name] }
+      prompts = menu_options.map { |item| item[:prompt] }
       selected = Utilities.rofi_select(items: prompts)
 
       return { action: :exit } if selected.nil?
 
-      launch_command = menu_options.find { |item| item[:name] == selected }&.dig(:command).to_s.capitalize
+      launch_command = menu_options.find { |item| item[:prompt] == selected }&.dig(:command).to_s.capitalize
       target = Menus.const_get(launch_command)
       { action: :push, target: target }
     end
